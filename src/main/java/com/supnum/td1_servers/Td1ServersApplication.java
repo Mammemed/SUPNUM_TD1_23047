aa871a7cf1908af90711d71ba370e77716b757dd
Mama-mouhamed <23047@supnum.mr>
2025-11-19 19:54:33 +0000

    Init projet Spring Boot TD1

---
+++ src/main/java/com/supnum/td1_servers/Td1ServersApplication.java
@@ -0,0 +1,13 @@
+package com.supnum.td1_servers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td1ServersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Td1ServersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supnum/td1_servers/Td1ServersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supnum.td1_servers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td1ServersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
